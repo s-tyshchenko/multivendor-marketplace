d57336bfc0fb616f520fa1d7669e6098d93e0060
Stan <s.tyshchenko@icloud.com>
2023-11-16 15:18:06 +0100

    Remove error die

--- platform/plugins/marketplace/src/Providers/OrderSupportServiceProvider.php
+++ platform/plugins/marketplace/src/Providers/OrderSupportServiceProvider.php
@@ -249,7 +249,6 @@ class OrderSupportServiceProvider extends ServiceProvider
         }
 
         if ($paymentData['error'] || ! $paymentData['charge_id']) {
-            die(var_dump($paymentData));
             return $response
                 ->setError()
                 ->setNextUrl(PaymentHelper::getCancelURL($token))
