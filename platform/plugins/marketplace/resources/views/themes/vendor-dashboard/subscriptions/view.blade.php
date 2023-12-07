40d8d5184508eed9b450c4e93bb2e466ceb777d3
Stan <s.tyshchenko@icloud.com>
2023-12-07 17:25:13 +0100

    Vendor remove cancel subsiption

--- platform/plugins/marketplace/resources/views/themes/vendor-dashboard/subscriptions/view.blade.php
+++ platform/plugins/marketplace/resources/views/themes/vendor-dashboard/subscriptions/view.blade.php
@@ -5,14 +5,6 @@
     <div class="card-body">
         <div class="customer-order-detail">
             @include('plugins/ecommerce::themes.includes.subscription-tracking-detail')
-
-            <div class="mt-4 row">
-                <div class="col-auto">
-                    <a href="{{ route('marketplace.vendor.subscriptions.cancel', ['id' => $subscription->id]) }}"
-                       onclick="return confirm('{{ __('Are you sure?') }}')"
-                       class="btn btn-danger btn-sm ml-2">{{ __('Cancel subscription') }}</a>
-                </div>
-            </div>
         </div>
     </div>
 </div>
